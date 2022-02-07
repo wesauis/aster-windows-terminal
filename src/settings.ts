eb0be7ac8bff7daa99f005c86556199c318e9859
Wesley Augusto Isotton <54046750+wesauis@users.noreply.github.com>
2022-02-07 08:32:19 -0300

    Use a dimmer white

--- src/settings.ts
+++ src/settings.ts
@@ -3,7 +3,6 @@ import mix from "./utils/mix";
 
 export const scheme: Scheme = {
   name: "Aster",
-  white: "#E8E5E3",
   black: "#141617",
   blue: "#719DF4",
   cyan: "#48D7C3",
@@ -11,7 +10,10 @@ export const scheme: Scheme = {
   red: "#DA5858",
   purple: "#CA60FB",
   yellow: "#F0C544",
-  brightWhite: "#FFFFFF",
+  brightWhite: "#E8E5E3",
+  get white() {
+    return mix(this.black, this.brightWhite, 0.85);
+  },
   get foreground() {
     return this.white;
   },
@@ -25,25 +27,25 @@ export const scheme: Scheme = {
     return this.black;
   },
   get brightBlack() {
-    return mix(this.black, this.white, 0.5);
+    return mix(this.black, this.brightWhite, 0.5);
   },
   get brightBlue() {
-    return mix(this.blue, this.white, 0.25);
+    return mix(this.blue, this.brightWhite, 0.25);
   },
   get brightCyan() {
-    return mix(this.cyan, this.white, 0.4);
+    return mix(this.cyan, this.brightWhite, 0.4);
   },
   get brightGreen() {
-    return mix(this.green, this.white, 0.25);
+    return mix(this.green, this.brightWhite, 0.25);
   },
   get brightRed() {
-    return mix(this.red, this.white, 0.25);
+    return mix(this.red, this.brightWhite, 0.25);
   },
   get brightPurple() {
-    return mix(this.purple, this.white, 0.4);
+    return mix(this.purple, this.brightWhite, 0.4);
   },
   get brightYellow() {
-    return mix(this.yellow, this.white, 0.25);
+    return mix(this.yellow, this.brightWhite, 0.25);
   },
 };
 
